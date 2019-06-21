19cfd22ad6c4d4e9a9b3dae0dc058a6ba05ce6d0
adam <bcuz@users.noreply.github.com>
2019-06-21 09:56:58 -0400

    render App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return <h1>hi</h1>;
+  }
 }
 
 export default App;
