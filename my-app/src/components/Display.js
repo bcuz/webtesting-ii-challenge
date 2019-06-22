8652a84c38d3dd230181629b7b52874e08adec06
adam <bcuz@users.noreply.github.com>
2019-06-22 14:37:09 -0400

    cleaning things up

--- my-app/src/components/Display.js
+++ my-app/src/components/Display.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 
 function Display(props) {
 
-  let { ball = 0, strike = 0 } = props
+  let { balls = 0, strikes = 0 } = props
 
     return (
       <div>
-        <p>balls: {ball}</p>
-        <p>strikes: {strike}</p>
+        <p>balls: {balls}</p>
+        <p>strikes: {strikes}</p>
       </div>
 
   )
